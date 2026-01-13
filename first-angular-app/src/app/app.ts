c790c1b5474ed00b338aa7128a741211010487c6
tamerlan053 <tamerlan.aslakhanov@gmail.com>
2026-01-13 16:03:54 +0300

    header component successfully realised

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('first-angular-app');
