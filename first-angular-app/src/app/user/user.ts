2f82a3828ff1cf7711c3bd7e8a227d84780a2808
tamerlan053 <tamerlan.aslakhanov@gmail.com>
2026-01-14 15:29:39 +0300

    signals implemented

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -7,18 +7,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
+  standalone: true,
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  });
 
   onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
     console.log('Ckicked');
-    this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
   }
 }
