ffb867a90c0225e6ba3cc16cb56cde52a2108c73
tamerlan053 <tamerlan.aslakhanov@gmail.com>
2026-01-14 14:25:19 +0300

    managing state & changing data

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('Ckicked');
+    this.selectedUser = DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)];
+  }
 }
