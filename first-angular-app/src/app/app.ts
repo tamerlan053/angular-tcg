5074f31ccb0ade1b639f86f15b4e5fb2ab6dd771
tamerlan053 <tamerlan.aslakhanov@gmail.com>
2026-01-14 12:31:24 +0300

    User component styled

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
